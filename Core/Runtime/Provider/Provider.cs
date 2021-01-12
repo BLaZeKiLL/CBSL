e102907df72fc093c29664f177e20a7df5166f76
BLaZeKiLL <blazeingamer10@gmail.com>
2021-01-13 00:27:34 +0530

    Core Lib Created

---
+++ Core/Runtime/Provider/Provider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CBSL.Core.Source.Core.Runtime.Provider {
+
+    public abstract class Provider<P, S> where P : Provider<P, S> where S : new() {
+
+        public static P Current { get; private set; }
+
+        public S Settings { get; set; }
+
+        public static void Initialize(Func<P> provider, S settings) {
+            Current = provider();
+            Current.Settings = settings;
+        }
+
+    }
+
+}
