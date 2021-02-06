1e150b20ade236158888adb3283ff18dd7b722dd
BLaZeKiLL <blazeingamer10@gmail.com>
2021-02-06 17:28:35 +0530

    compressed data access added

--- Core/Runtime/Collections/Compressed/CompressedArray.cs
+++ Core/Runtime/Collections/Compressed/CompressedArray.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using UnityEngine;
+
 namespace CBSL.Core.Collections.Compressed {
 
     public abstract class CompressedArray<T> {
@@ -29,6 +31,26 @@ namespace CBSL.Core.Collections.Compressed {
 
         protected abstract byte[] GetBytes(T obj);
 
+        public T GetAt(int index) {
+            return State switch {
+                DataState.COMPRESSED => GetInCompressed(index),
+                DataState.DECOMPRESSED => GetDeCompressedData()[index],
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public void SetAt(int index, T obj) {
+            switch (State) {
+                case DataState.COMPRESSED:
+                    throw new NotImplementedException("Set at for compressed data not implemented");
+                case DataState.DECOMPRESSED:
+                    GetDeCompressedData()[index] = obj;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         public List<byte> GetCompressedData() {
             if (State != DataState.COMPRESSED) throw new InvalidOperationException("Data not compressed");
 
@@ -45,22 +67,21 @@ namespace CBSL.Core.Collections.Compressed {
             var data = GetDeCompressedData();
             var bytes = new List<byte>();
             var cdata = data[0];
-            var index = 1;
+            var index = 0;
             
             bytes.AddRange(GetBytes(cdata));
 
             for (int i = 1; i < data.Length; i++) {
-                if (data[i].Equals(cdata)) {
-                    index++;
-                } else {
-                    bytes.AddRange(BitConverter.GetBytes(index));
-                    index = 1;
-                    cdata = data[i];
-                    bytes.AddRange(GetBytes(cdata));
-                }
+                index++;
+
+                if (data[i].Equals(cdata)) continue;
+                
+                bytes.AddRange(BitConverter.GetBytes(index));
+                cdata = data[i];
+                bytes.AddRange(GetBytes(cdata));
             }
 
-            bytes.AddRange(BitConverter.GetBytes(index));
+            bytes.AddRange(BitConverter.GetBytes(++index));
 
             _data = bytes;
             State = DataState.COMPRESSED;
@@ -76,23 +97,43 @@ namespace CBSL.Core.Collections.Compressed {
                 var obj = FromBytes(bytes.GetRange(i, DataSize).ToArray());
                 var count = BitConverter.ToInt32(bytes.GetRange(i + DataSize, sizeof(int)).ToArray(), 0);
 
-                for (int j = bindex; j < count + bindex; j++) {
+                for (int j = bindex; j < count; j++) {
                     data[j] = obj;
                 }
 
-                bindex += count;
+                bindex = count;
             }
 
             _data = data;
             State = DataState.DECOMPRESSED;
         }
-        
+
+        private T GetInCompressed(int index) {
+            var data = GetCompressedData();
+            var min = 0;
+            var max = Mathf.CeilToInt((float) data.Count / (DataSize + sizeof(int)));
+
+            while (min <= max) {
+                var mid = (max + min) / 2;
+                var val = BitConverter.ToInt32(data.GetRange(mid * (DataSize + sizeof(int)) + DataSize, sizeof(int)).ToArray(), 0);
+
+                if (index == val) return FromBytes(data.GetRange((mid + 1) * (DataSize + sizeof(int)), DataSize).ToArray());
+
+                if (index < val) max = mid - 1;
+                else min = mid + 1;
+            }
+            
+            return FromBytes(data.GetRange(min * (DataSize + sizeof(int)), DataSize).ToArray());
+        }
+
         public enum DataState {
 
             COMPRESSED,
             DECOMPRESSED
 
         }
+        
+        
 
     }
 
