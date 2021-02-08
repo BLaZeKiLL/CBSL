0ffa91a410cb8af781557ae2618e6f6aa3bbdfc7
BLaZeKiLL <blazeingamer10@gmail.com>
2021-02-08 10:10:11 +0530

    build fix

--- cbsl/docusaurus.config.js
+++ cbsl/docusaurus.config.js
@@ -41,7 +41,7 @@ module.exports = {
           items: [
             {
               label: 'Getting Started',
-              to: 'docs/',
+              to: '/',
             }
           ],
         },
