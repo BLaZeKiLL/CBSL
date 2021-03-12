946abe999d7555b999d20a6990a4017e63f0f9fb
BLaZeKiLL <blazeingamer10@gmail.com>
2021-03-12 17:38:50 +0530

    unity updated to 2020.3 LTS

---
+++ Manager/Runtime/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace CBSL.ManagerFramework {
+    
+    public class Manager {
+
+        protected virtual void Start() {}
+        
+        protected virtual void Update() {}
+        
+        protected virtual void LateUpdate() {}
+        
+        protected virtual void DrawGizmos() {}
+        
+        protected virtual void Destroy() {}
+
+    }
+
+}
---
+++ Manager/Runtime/ManagerController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace CBSL.ManagerFramework {
+
+    public class ManagerController : MonoBehaviour {
+
+        [SerializeField] private List<Manager> _managers;
+        
+        // Start is called before the first frame update
+        private void Start() { }
+
+        // Update is called once per frame
+        private void Update() { }
+
+    }
+
+}
