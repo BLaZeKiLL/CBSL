9901b38e9cc5c6bc419ee3010f7a5004a000d218
BLaZeKiLL <blazeingamer10@gmail.com>
2021-03-13 16:44:06 +0530

    Manager framework implemented

---
+++ Packages/CBSL/Manager/Runtime/Scene/SceneController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace CBSL.ManagerFramework.Scene {
+
+    [DefaultExecutionOrder(-1200)]
+    public abstract class SceneController : MonoBehaviour {
+
+        public static event EventHandler PostBootstrap; 
+        
+        protected abstract void Bootstrap();
+
+        private void Start() {
+            Bootstrap();
+            PostBootstrap?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+
+}
